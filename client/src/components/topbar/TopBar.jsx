ccb76a399c155b6d376802282a62b75248d2d7ac
Night Sailor <sulemanshah432@gmail.com>
2021-10-03 12:18:06 +0000

    make it better 4

--- client/src/components/topbar/TopBar.jsx
+++ client/src/components/topbar/TopBar.jsx
@@ -13,26 +13,15 @@ export default function TopBar() {
   return (
     <div className="top">
       <div className="topLeft">
-        <i className="topIcon fab fa-facebook-square"></i>
-        <i className="topIcon fab fa-twitter-square"></i>
-        <i className="topIcon fab fa-pinterest-square"></i>
-        <i className="topIcon fab fa-instagram-square"></i>
+        <Link className="link" to="/">
+          Petrature
+        </Link>
       </div>
       <div className="topCenter">
         <ul className="topList">
           <li className="topListItem">
             <Link className="link" to="/">
               HOME
-            </Link>
-          </li>
-          <li className="topListItem">
-            <Link className="link" to="/">
-              ABOUT
-            </Link>
-          </li>
-          <li className="topListItem">
-            <Link className="link" to="/">
-              CONTACT
             </Link>
           </li>
           <li className="topListItem">
